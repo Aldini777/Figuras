28ae6061f9cb7c3e80d80f1a5482441fb95bc504
Aldo Resendiz Cravioto <133988006+Aldini777@users.noreply.github.com>
2023-11-10 15:51:48 -0600

    Update exercise.cpp

--- assignments/Figuras/exercise.cpp
+++ assignments/Figuras/exercise.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include "Circuito.hpp"
+using namespace std;
+
 
 int main() 
 {
-  std::cout << "Hello World \n";
+  int x = 5;
+  int& refX = x
+  cout << "Valor de x: " << x << "Valor referencia de x: " << refX << endl;
+  refX = 10
+  cout << "Valor de x: " << x << "Valor referencia de x: " << refX << endl;
+  cout << "Direccion de memoria: " << &x << "Direccion de refX: " << &refX << endl;
+  //Apuntadores
+  int* ptrX;
+  ptrX = &x;
+  cout << "Lo que guarda el ptrX: " << ptrX << "El valor al que apunta: " << *ptrX <<endl;
+  int* ptrDin = new int(20)
+  cout << "Lo que guarda el ptrDin: " << ptrDin << "El valor al que apunta: " << *ptrDin << endl;
+  delete ptrDin;
+  cout << "Lo que guarda el ptrDin: " << ptrDin << "El valor al que apunta: " << *ptrDin << endl;
+  return 0;
 }
